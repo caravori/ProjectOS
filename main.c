3b706091ab31ca2a922c2fcfccdea2d536332f57
caravori <gabrielscavazini@gmail.com>
2022-07-14 13:43:24 -0300

    fileHandler

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include "process.h"
+#include "fileHandler.h"
 
 void menu();
 
 int main (void){
     g_clock = 0;
-    pcb *new = malloc (sizeof(pcb));
-    pcb *newNew = malloc (sizeof(pcb));
-    //printf("Sizeof pcb:%ld, sizeof memory:%ld",sizeof(pcb),sizeof(memory));
-    new->pid = 1;
-    pcb head = newNode(new,newNew);
-    delNode(new,1);   
+    //printf("Sizeof pcb:%ld, sizeof memory:%ld",sizeof(pcb),sizeof(memory));   
     return 0;
 }
 
